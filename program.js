032355e11d2e8fdd914f641d6fe3b47485c06402
HovSkay <32835549+Hovsky@users.noreply.github.com>
2018-07-03 00:48:29 +0200

    repaired the code
    repaired the wrong code, made few tweaks. Next step is to add 2 more elements and program them also

--- program.js
+++ program.js
@@ -1,5 +1,6 @@
 function izracunaj() {
-    var usteda = prihodGodisnji * (efikasnost/100 + krada);
+
+    var usteda = prihodGodisnji * (efikasnost / 100 + krada);
     var zaradaPoStolu = usteda / brojStolova;
     var investicija = cijenaStola * brojStolova;
     var mjesecnaRata = investicija * 0.5 / 12;
@@ -9,73 +10,74 @@ function izracunaj() {
     document.getElementById("sveukupnaUsteda").innerHTML = sveukupnaUsteda + " kn";
 }
 
+function sliderChange() {
+    prihodGodisnji = prihodSlider.value * 1000000;
+    prihodKocka.value = prihodGodisnji / 1000000;
 
-//inicijalizacija
+    brojKonobara = konobarSlider.value;
+    konobarKocka.value = brojKonobara;
+
+    brojStolova = stoloviSlider.value;
+    stoloviKocka.value = brojStolova;
+
+    efficiency(brojKonobara, prihodGodisnji);
+}
+
+function numberBox() {
+    prihodGodisnji = prihodKocka.value * 1000000;
+    prihodSlider.value = prihodGodisnji / 1000000;
+
+    brojKonobara = konobarKocka.value;
+    konobarSlider.value = brojKonobara;
+
+    brojStolova = stoloviKocka.value;
+    stoloviSlider.value = brojStolova;
+
+    efficiency(brojKonobara, prihodGodisnji);
+}
+
+function efficiency(konobar, prihod) {
+    efikasnost = ((konobar * placa * 12) / prihod) * 0.5 * 100;
+    efikasnost = efikasnost.toFixed(2);
+    document.getElementById("postotakEfikasnost").innerHTML = efikasnost + "%";
+}
+
+
+//prihvati sve elemente
 var prihodKocka = document.getElementById("prihodBroj");
 var konobarKocka = document.getElementById("konobarBroj");
 var stoloviKocka = document.getElementById("stoloviBroj");
 
-var izracunKraj = document.getElementById("izracunKraj");
 var prihodSlider = document.getElementById("prihod");
 var konobarSlider = document.getElementById("konobar");
 var stoloviSlider = document.getElementById("stolovi");
+
+var izracunKraj = document.getElementById("izracunKraj");
+
+//postavi početne brojeve
 var prihodGodisnji = prihodSlider.value * 1000000;
 var brojKonobara = konobarSlider.value;
 var brojStolova = stoloviSlider.value;
 
+prihodKocka.value = prihodSlider.value;
+konobarKocka.value = konobarSlider.value;
+stoloviKocka.value = stoloviSlider.value;
 var placa = 4000;
-var efikasnost = (((brojKonobara*placa*12)/prihodGodisnji)*0.5).toFixed(2);
 var krada = 0.1;
 var cijenaStola = 20000;
 
-prihodKocka.value = prihodSlider.value * 1000000;
-konobarKocka.value = konobarSlider.value;
-stoloviKocka.value = stoloviSlider.value;
+efficiency(brojKonobara, prihodGodisnji);
+
 
 document.getElementById("postotakKrada").innerHTML = krada * 100 + "%";
-document.getElementById("postotakEfikasnost").innerHTML = efikasnost * 100 + "%";
-
+document.getElementById("postotakEfikasnost").innerHTML = efikasnost + "%";
 
 //Slider dio koda
-prihodSlider.addEventListener("change", function () {
-    prihodGodisnji = prihodSlider.value * 1000000;
-    prihodKocka.value = prihodSlider.value * 1000000;
-})
+sliders = document.querySelectorAll('.sliderDot');
+sliders.forEach(slider => slider.addEventListener('input', sliderChange));
 
-konobarSlider.addEventListener("change", function () {
-    brojKonobara = konobarSlider.value;
-    efikasnost = ((brojKonobara*placa*12)/prihodGodisnji)*0.5*100;
-    efikasnost = efikasnost.toFixed(2);
-    document.getElementById("postotakEfikasnost").innerHTML =efikasnost + "%";
-    konobarKocka.value = konobarSlider.value; 
-})
+//numberBox dio koda
+numbers = document.querySelectorAll('.numberDot');
+numbers.forEach(number => number.addEventListener('change', numberBox));
 
-stoloviSlider.addEventListener("change", function () {
-    brojStolova = stoloviSlider.value;
-    stoloviKocka.value = stoloviSlider.value;
-})
-
-//number input dio koda
-prihodKocka.addEventListener("change", function() {
-    prihodSlider.value = prihodKocka.value / 1000000;
-})
-
-konobarKocka.addEventListener("change", function() {
-    konobarSlider.value = konobarKocka.value;
-    if(konobarKocka.value>30) {
-        efikasnost = 0.04;
-    }
-    else if(konobarKocka.value<10) {
-        efikasnost = 0.02;
-    }
-    else {
-        efikasnost = 0.03;
-    }
-    document.getElementById("postotakEfikasnost").innerHTML = efikasnost * 100 + "%";
-})
-
-stoloviKocka.addEventListener("change", function() {
-    stoloviSlider.value = stoloviKocka.value;
-})
-
-
+document.getElementById('Calculate').addEventListener('click', izracunaj);
